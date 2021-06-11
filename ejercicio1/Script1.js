680db07fcce5edef53144b2cbc72bf9f7940ed5c
Agustin Tottil <84998888+LeaveKou@users.noreply.github.com>
2021-06-11 18:13:24 -0300

    Add files via upload

---
+++ ejercicio1/Script1.js
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+// JavaScript source code
+let Nota = window.prompt("Ingrese su nota (0-10)");
+if (Nota > 10 || Nota < 0 || Nota == NaN) {
+    alert("Ingreso un dato erroneo");
+}
+else {
+    if (Nota < 3 && Nota > -1) { alert("Muy Deficiente"); }
+    if (Nota < 5 && Nota > 2) { alert("Insuficiente"); }
+    if (Nota < 7 && Nota > 4) { alert("Suficiente"); }
+    if (Nota < 8 && Nota > 6) { alert("Bien"); }
+    if (Nota < 9 && Nota > 7) { alert("Notable"); }
+    if (Nota < 11 && Nota > 8) { alert("Sobresaliente"); }
+=======
+// JavaScript source code
+let Nota = window.prompt("Ingrese su nota (0-10)");
+if (Nota > 10 || Nota < 0 || Nota == NaN) {
+    alert("Ingreso un dato erroneo");
+}
+else {
+    if (Nota < 3 && Nota > -1) { alert("Muy Deficiente"); }
+    if (Nota < 5 && Nota > 2) { alert("Insuficiente"); }
+    if (Nota < 7 && Nota > 4) { alert("Suficiente"); }
+    if (Nota < 8 && Nota > 6) { alert("Bien"); }
+    if (Nota < 9 && Nota > 7) { alert("Notable"); }
+    if (Nota < 11 && Nota > 8) { alert("Sobresaliente"); }
+>>>>>>> 53bd5b9e02876f58a67a7bc664a56b074bb9b5e7
+}
